9933b3cbb5cb8011d5091648817955bfe9aaeed5
Yoctoboy <alexis@padam.io>
2020-06-03 12:21:09 +0200

    draw the waves dynamically instead of in setup() function

--- simple_waves/sketch.js
+++ simple_waves/sketch.js
@@ -6,6 +6,9 @@ const noise_speed_factor = 0.5;
 // not a reachable value so you may want to increase this in order to get bigger y moves
 const min_max_y_speed = 3;
 
+let cur_x, max_x;
+let x_speed, y_speed;
+
 
 
 
@@ -18,7 +21,7 @@ function setup() {
   noise_module.seed(random(0, 100000));
 
   blendMode(BLEND);
-  amount = Math.floor(random(1, 28));
+  amount = Math.floor(random(2, 15));
   create_waves(amount);
 }
 
@@ -49,11 +52,11 @@ function create_waves(amount) {
   /* define waves starting positions */
 
   // ensure the final drawing will be horinzontally centered
-  cur_x = max(0, min(0.45 * width, randomGaussian(width / 4, 150)));
+  cur_x = max(0, min(0.45 * width, randomGaussian(width / 6, 150)));
   max_x = width - cur_x;
 
   // ensure the final drawing will be vertically centered
-  y_offset = random(8, 18);
+  y_offset = random(12, 35);
   middle_y = height / 2;
   base_y = middle_y - (amount / 2 * y_offset);
 
@@ -70,18 +73,18 @@ function create_waves(amount) {
     all_waves.push(new Wave(cur_x, base_y + i * y_offset, col, thickness));
   }
 
-  // create a random line with perlin noise
-  x_speed = 5;
-  y_speed = random(-1, 1);
-  while (cur_x < max_x) {
-    all_waves.forEach(wave => wave.draw(x_speed, y_speed));
-    y_speed = min_max_y_speed * noise_module.perlin3(0.005 * cur_x, 0.002 * y_speed, random(0, 0.002));
-    y_speed = min(min_max_y_speed, max(-min_max_y_speed, y_speed));
-    cur_x += x_speed;
-  }
+  y_speed = random(-2, 2);
+  x_speed = 15;
 }
 
 
 function draw() {
   // put drawing code here
+  // create a random line with perlin noise
+  if (cur_x < max_x) {
+    all_waves.forEach(wave => wave.draw(x_speed, y_speed));
+    y_speed = min_max_y_speed * noise_module.perlin3(0.005 * cur_x, 0.002 * y_speed, random(0, 0.001));
+    y_speed = min(min_max_y_speed, max(-min_max_y_speed, y_speed));
+    cur_x += x_speed;
+  }
 }
