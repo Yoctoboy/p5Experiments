aecd4a3c379fda098fb204f0b5aa6cb419da7283
Yoctoboy <alexis@padam.io>
2020-09-07 14:17:30 +0200

    refactor drawing functions so we can use them more than once

--- album_cover_1/sketch.js
+++ album_cover_1/sketch.js
@@ -7,6 +7,8 @@
 
 
 let all_branches = [];
+let branches_amount = 1000;
+const max_steps = 10000;
 
 
 function setup() {
@@ -16,42 +18,60 @@ function setup() {
   colorMode(RGB);
 
   // ensure the generated pattern is different on each page refresh
-  // const perlin_seed = Math.floor(random(0, 900000));
-  const perlin_seed = 468354;
-  // GOOD SEEDS: 468354, 511720
-  console.log(perlin_seed)
+  // GOOD SEEDS: (65460 334034); (591106 491244); (840189 251902)
+  const perlin_seed = Math.floor(random(0, 900000));
+  // const perlin_seed = 787043;
   noise_module.seed(perlin_seed);
+  const random_seed = Math.floor(random(0, 900000));
+  // const random_seed = 754521;
+  randomSeed(random_seed)
+  console.log(perlin_seed, random_seed)
 
-  create_branches(1000);
+  let x = - width / 20;
+  let y = height;
+  let min_speedy = -1.5;
+  let max_speedy = -1;
+  let min_speedx = 3;
+  let max_speedx = 5;
+  branches = create_branches(branches_amount, x, y, min_speedx, max_speedx, min_speedy, max_speedy);
+  draw_branches(branches);
 
   stroke(255);
   strokeWeight(1);
   blendMode(BLEND);
 }
 
+function draw_branches(branches) {
+  for (let step = 0; step < max_steps; step++) {
+    branches.forEach(branch => {
+      branch.move(step);
+      branch.draw();
+      branch.update_visible();
+    })
+  }
+}
+
 // higher factor makes the branches faster and makes them stay more grouped
 // recommended range: [0.1, 10]
-const noise_speed_factor = 0.2;
+const noise_speed_factor = 0.25;
 
-const min_speedy = -2;
-const max_speedy = -1;
 
 class Branch {
-  constructor(x, y) {
+  constructor(x, y, min_speedx, max_speedx, min_speedy, max_speedy) {
     this.x = x
     this.y = y
     this.prevx = x;
     this.prevy = y;
-    this.speedx = random(2, 3);
+    this.speedx = random(min_speedx, max_speedx);
     this.speedy = random(min_speedy, max_speedy);
     this.color = color(map(this.speedy, min_speedy, max_speedy, 120, 220), 0, 100, 40);
     this.visible = true;
   }
 
-  move() {
+  move(step) {
     // updates current position of the branch
-    this.speedx += noise_speed_factor * noise_module.simplex3(this.x * 0.002, this.y * 0.002, millis() * 0.001);
-    this.speedy += noise_speed_factor * noise_module.simplex3(this.y * 0.002, this.x * 0.002, millis() * 0.001);
+    this.speedx += noise_speed_factor * noise_module.simplex3(this.x * 0.002, this.y * 0.002, step * 0.00001);
+    this.speedy += noise_speed_factor * noise_module.simplex3(this.y * 0.002, this.x * 0.002, step * 0.00001);
     this.x += this.speedx;
     this.y += this.speedy;
   }
@@ -72,15 +92,12 @@ class Branch {
   }
 }
 
-function create_branches(amount) {
-  all_branches = [];
+function create_branches(amount, x, y, min_speedx, max_speedx, min_speedy, max_speedy) {
+  let branches = [];
   for (let i = 0; i < amount; i++) {
-    const x = - width / 20;
-    const y = height;
-    //const x = width / 2;
-    //const y = height / 2;
-    all_branches.push(new Branch(x, y));
+    branches.push(new Branch(x, y, min_speedx, max_speedx, min_speedy, max_speedy));
   }
+  return branches;
 }
 
 function draw() {
@@ -88,5 +105,6 @@ function draw() {
     branch.move();
     branch.draw();
     branch.update_visible();
+    step += 1;
   })
 }
