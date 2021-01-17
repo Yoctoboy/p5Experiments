edd0c158ff5a03de700e9bff792d2a793592a633
Yoctoboy <alexis@padam.io>
2021-01-17 16:58:09 +0100

    add base canvas size code in empty_template

--- empty_template/sketch.js
+++ empty_template/sketch.js
@@ -1,6 +1,15 @@
+var canvas_width = 800;
+var canvas_height = 600;
+
 function setup() {
+  createCanvas(canvas_width, canvas_height);
+
+  background(0);
+  colorMode(RGB);
+
   // put setup code here
 }
+
 
 function draw() {
   // put drawing code here
