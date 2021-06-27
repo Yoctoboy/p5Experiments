19eb3424ffc94e7cb0deac07ac3bfac02266280a
Yoctoboy <alexis@padam.io>
2021-06-27 18:45:56 +0200

    add random seeding function in template sketch

--- empty_template/sketch.js
+++ empty_template/sketch.js
@@ -1,11 +1,23 @@
 var canvas_width = 800;
 var canvas_height = 600;
 
+
+function seed_random_modules(){
+  const random_seed = Math.floor(random(0, 900000));
+  const perlin_seed = Math.floor(random(0, 900000));
+  randomSeed(random_seed);
+  noise_module.seed(perlin_seed)
+  console.log("Random seed =", random_seed, "/ Perlin seed =", perlin_seed);
+}
+
+
 function setup() {
   createCanvas(canvas_width, canvas_height);
 
   background(0);
   colorMode(RGB);
+
+  seed_random_modules()
 
   // put setup code here
 }
