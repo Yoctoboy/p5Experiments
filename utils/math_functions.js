59afa9495a38df789586892e660c1cdaa7366ad6
Yoctoboy <alexis@padam.io>
2021-06-27 16:11:37 +0200

    update template to use perlin noise functions

--- utils/math_functions.js
+++ utils/math_functions.js
@@ -1,4 +1,14 @@
 function randomExponential(lambda){
-    var randomUniform = Math.random()
+    var randomUniform = Math.random();
     return -Math.log(randomUniform)/lambda;
+}
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+function get_orthogonal_vector(vector, normalize=true){
+    orthogonal_vector = createVector(1, -vector.x / vector.y);
+    if (normalize) orthogonal_vector.normalize();
+    return orthogonal_vector;
 }
