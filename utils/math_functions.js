046008f0bab7ba747672c34d7ec596761b74ec56
Yoctoboy <alexis@padam.io>
2021-09-22 19:28:27 +0200

    more utils

--- utils/math_functions.js
+++ utils/math_functions.js
@@ -7,6 +7,11 @@ function randomInterval(min, max) {
     return Math.random() * (max - min) + min;
 }
 
+function randInt(min, max){
+    // random integer between min and max (included)
+    return Math.floor(Math.random() * (max + 1 - min) + min);
+}
+
 function get_orthogonal_vector(vector, normalize=true){
     orthogonal_vector = createVector(1, -vector.x / vector.y);
     if (normalize) orthogonal_vector.normalize();
